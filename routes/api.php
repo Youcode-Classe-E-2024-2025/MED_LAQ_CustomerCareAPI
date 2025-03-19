b68de3ad1a801e6586e2e3489592b64f16d5523c
medlaq777 <m.laqsoumi@gmail.com>
2025-03-19 21:43:19 +0000

    add the updateUser and deleteUser functions into the service user

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,11 @@ use App\Services\UserService;
 Route::post('/users', function (Request $request, UserService $userService) {
     return $userService->createUser($request->all());
 });
+
+Route::put('/users/{id}', function (Request $request, $id, UserService $userService) {
+    return $userService->updateUser($id, $request->all());
+});
+
+Route::delete('/users/{id}', function ($id, UserService $userService) {
+    return $userService->deleteUser($id);
+});
