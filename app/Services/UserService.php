9a4d4f98491ca336c0921748ed67dbd56bbe4bf8
medlaq777 <m.laqsoumi@gmail.com>
2025-03-19 20:58:09 +0000

    Use the UserService in UserController

---
+++ app/Services/UserService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserService
+{
+    public function createUser(array $data)
+    {
+        $validator = Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
+        }
+
+        $data['password'] = Hash::make($data['password']);
+
+        return User::create($data);
+    }
+}
