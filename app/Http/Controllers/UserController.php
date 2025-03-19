a2f7f9d8fba4dce6c4672a790ed8babfc65b57bf
medlaq777 <m.laqsoumi@gmail.com>
2025-03-19 20:56:24 +0000

    Use the UserService in a Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Services\UserService;
 
 class UserController extends Controller
 {
+    protected $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
 }
