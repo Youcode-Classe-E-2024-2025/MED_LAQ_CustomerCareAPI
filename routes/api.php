c1c8a29c43a81f9852e46dfab528b50a2697ac4b
medlaq777 <m.laqsoumi@gmail.com>
2025-03-22 21:42:25 +0000

    remake the project

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,3 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Services\UserService;
-
-Route::post('/users', function (Request $request, UserService $userService) {
-    return $userService->createUser($request->all());
-});
-
-Route::put('/users/{id}', function (Request $request, $user , UserService $userService) {
-    return $userService->updateUser($user, $request->all());
-});
-
-Route::delete('/users/{id}', function ($user, UserService $userService) {
-    return $userService->deleteUser($user);
-});
