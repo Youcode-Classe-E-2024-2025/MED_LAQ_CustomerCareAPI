3de8c29d6cea8851d2becfaa154523bafcb6a14e
medlaq777 <m.laqsoumi@gmail.com>
2025-03-27 23:04:29 +0000

    fix the routing for Login and Register ract client side with add npm tailwind

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/Login', function () {
     return inertia('Login');
@@ -9,3 +10,7 @@ Route::get('/Login', function () {
 Route::get('/Register', function () {
     return inertia('Register');
 });
+
+
+Route::post('/Login', [AuthController::class, 'Login']);
+Route::post('/Register', [AuthController::class, 'Register']);
